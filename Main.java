555085ed240ef225cd53207f5aac4db2f55ea42d
sushil neupane <sushilbandhuu@gmail.com>
2024-11-13 09:27:28 +0545

    java ko class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("java"s object oriented language);
+        
+
+
+        // String myname="sushil";
+        // System.out.println(myname);
+    }
+}
